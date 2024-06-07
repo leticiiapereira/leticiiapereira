b09ba6b11dc2cec91f3c2030d8ce2eedf84bb880
leticiiapereira <00001102534298sp@al.educacao.sp.gov.br>
2024-06-07 16:02:26 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,42 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+let raio = diametro /2;
+
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+    movimentaBolinha();
+    verificaColisaoBorda();
+}
+
+function mostraBolinha() {
+    circle(xBolinha, yBolinha, diametro)
+}
+
+function movimentaBolinha() {
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+}
+  
+  
+  circle(xBolinha, yBolinha, diametro);
+function movimentaBolinha () 
+  xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;}
+
+  function verificaColisaoBorda() {
+  if(xBolinha > width || xBolinha < 0)
+    velocidadeXBolinha*= -1;
+  }
+
+  if (yBolinha + raio > height || yBolinha - raio < 0) {
+        velocidadeYBolinha *= -1;}
 
